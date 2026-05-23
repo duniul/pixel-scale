b9613c452bd2783215ecae642945c7f1b28f3e99
Daniel Grefberg <hello@danielgrefberg.com>
2026-05-23 18:28:20 +0200

    chore: remove husky and lint-staged

--- .lintstagedrc.cjs
+++
@@ -1,7 +0,0 @@
-const prettierFormat = 'prettier --write';
-const biomeLintAndFormat = 'biome check --apply';
-
-module.exports = {
-  '*.{md,html,yaml,yml,css,scss}': [prettierFormat],
-  '*.{ts,tsx,js,jsx,json}': [biomeLintAndFormat],
-};
