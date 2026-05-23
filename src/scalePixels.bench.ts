048ab6b4fced2dc483161d32408ca0b19bf87be9
Daniel Grefberg <hello@danielgrefberg.com>
2026-05-23 23:33:57 +0200

    chore: update benchmark to include auto-detected scale

--- src/scalePixels.bench.ts
+++ src/scalePixels.bench.ts
@@ -9,10 +9,17 @@ globalThis.ImageData = IsomorphicImageData as any;
 const scale1 = await getPixelTheCatScale1();
 
 describe(scalePixels, () => {
-  for (const to of [5, 10, 20, 50] as const) {
-    bench(`x1 -> x${to}`, () => {
+  for (const to of [10, 20, 50, 100] as const) {
+    bench(`x1 -> x${to} (with from)`, () => {
       const scaled = scalePixels(scale1, to, { from: 1 });
       scalePixels(scaled, 1, { from: to });
+    });
+  }
+
+  for (const to of [10, 20, 50, 100] as const) {
+    bench(`x1 -> x${to} (auto-detect scale)`, () => {
+      const scaled = scalePixels(scale1, to);
+      scalePixels(scaled, 1);
     });
   }
 });
